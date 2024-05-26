7312983e64a63291cca3d581cf3453c5e56f175c
Rutuja Mhatre <59094217+rutuja1302@users.noreply.github.com>
2024-05-26 14:06:06 +0530

    Load Balancing with API Gateway

--- city-microservice/src/main/java/com/example/city_microservice/Configurations/HotelLoadBalancerConfig.java
+++ city-microservice/src/main/java/com/example/city_microservice/Configurations/HotelLoadBalancerConfig.java
@@ -1,17 +1,20 @@
-package com.example.city_microservice.Configurations;
-
-import feign.Feign;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
-import org.springframework.context.annotation.Bean;
-
-@LoadBalancerClient(value = "hotel-application") //service name of target server: same as feignclient
-public class HotelLoadBalancerConfig {
+//API Gateway by default handles load balancing for us - hence no need to manually configure load balancers
 
 
-    @LoadBalanced
-    @Bean
-    public Feign.Builder feignBuilder(){
-        return Feign.builder();
-    }
-}
+//package com.example.city_microservice.Configurations;
+//
+//import feign.Feign;
+//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+//import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+//import org.springframework.context.annotation.Bean;
+//
+//@LoadBalancerClient(value = "hotel-application") //service name of target server: same as feignclient
+//public class HotelLoadBalancerConfig {
+//
+//
+//    @LoadBalanced
+//    @Bean
+//    public Feign.Builder feignBuilder(){
+//        return Feign.builder();
+//    }
+//}
